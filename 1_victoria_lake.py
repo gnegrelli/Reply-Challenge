28362e61792421fb9fe696bf616e0d1750cb3a35
Gabriel Negrelli <gabriel.jose.gomes@usp.br>
2019-03-15 13:08:12 -0300

    Read info for each HQ and store in a list

--- 1_victoria_lake.py
+++ 1_victoria_lake.py
@@ -13,5 +13,14 @@ dic['map_size'] = (float(first_row[0]), float(first_row[1]))
 dic['Customer_HQ'] = float(first_row[2])
 dic['Reply_Offices'] = float(first_row[3])
 
+HQ = []
+
+for i in range(int(dic['Customer_HQ'])):
+    row_info = dataList[i+1].split(" ")
+    HQ.append([(float(row_info[0]), float(row_info[1])), float(row_info[2])])
+
+print HQ[0]
+
+
 # for eachLine in dataList:
     # print eachLine
