e230e861522537418ace7c1ca14966617e631f0f
Gabriel Negrelli <gnegrelli13@gmail.com>
2022-11-15 18:35:54 -0500

    Read data from map file

--- office_allocator.py
+++ office_allocator.py
@@ -1,6 +1,10 @@
+import random
 from argparse import ArgumentParser
 from pathlib import Path
 from typing import Any, Dict
+
+from world import World
+from office import Office
 
 
 def validate_args(args: ArgumentParser) -> Dict[str, Any]:
@@ -18,8 +22,26 @@ def validate_args(args: ArgumentParser) -> Dict[str, Any]:
     return {'map_file': map_file, }
 
 
+def read_map_file(file: Path) -> Dict[str, Any]:
+    with open(file, 'r') as file:
+        map_width, map_height, n_customers, n_offices = map(int, file.readline().strip().split(' '))
+
+        world = World(map_height, map_width)
+
+        customers = []
+        for _ in range(n_customers):
+            customer_x, customer_y, customer_reward = map(int, file.readline().strip().split(' '))
+            customers.append(Office(customer_x, customer_y, customer_reward))
+
+        world.add_world_terrain(file.read())
+        world.view_map()
+
+    return {'world': world, 'customers': customers, 'n_offices': n_offices, }
+
+
 if __name__ == '__main__':
     parser = ArgumentParser(description='Script to allocate offices on a map.')
     parser.add_argument('map_file', help='File containing map of terrain')
 
     args = validate_args(parser)
+    read_map_file(args['map_file'])
