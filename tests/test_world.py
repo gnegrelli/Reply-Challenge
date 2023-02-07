1d929bc90e580db3853137a3d105991e77562bb0
Gabriel Negrelli <gnegrelli13@gmail.com>
2023-02-07 11:54:31 -0500

    Update function called in test

--- tests/test_world.py
+++ tests/test_world.py
@@ -10,14 +10,14 @@ class TestWorld(unittest.TestCase):
         world = World(3, 3)
         world.add_world_terrain('###HHHTTT')
 
-        world.dijkstra((1, 1), (2, 2))
+        world.calculate_path_cost((1, 1), (2, 2))
 
     def test_dijkstra_1(self):
 
         world = World(3, 3)
         world.add_world_terrain('TT#~##~~~')
 
-        world.dijkstra((0, 0), (2, 2))
+        world.calculate_path_cost((0, 0), (2, 2))
 
     def test_dijkstra_2(self):
 
@@ -25,7 +25,7 @@ class TestWorld(unittest.TestCase):
         world.add_world_terrain('T#######~')
 
         with self.assertRaises(Exception):
-            world.dijkstra((0, 0), (2, 2))
+            world.calculate_path_cost((0, 0), (2, 2))
 
 
 if __name__ == '__main__':
